5fbde27525a36090cd6a10b47086e3a92ff071bd
alirast <xdabnormal@gmail.com>
2023-10-31 12:18:29 +0300

    Initial Commit

---
+++ RMapi/RMapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMapiApp.swift
+//  RMapi
+//
+//  Created by N S on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RMapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
